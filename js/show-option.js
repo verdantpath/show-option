4054d9cdee31f2fff1b6a41d59d03f0348f6812d
verdantpath <m_dittmann@yahoo.com>
2017-06-27 13:47:48 -0700

    update js

--- js/show-option.js
+++ js/show-option.js
@@ -6,12 +6,15 @@
   otherText = document.getElementById('other-text');
   otherText.className = 'hide';
 
+  // Loop through each radio button, add an event listener, when one is clicked call the radioChanged function
   for (var i = [0]; i < options.length; i++) {
     addEvent(options[i], 'click', radioChanged);
   }
+  // The class for 'other' text field is updated to 'hide' if not selected and blank if selected
   function radioChanged() {
     hide = other.checked ? '' : 'hide';
     otherText.className = hide;
+    // If the other radio is hidden, the text input is emptied
     if (hide) {
       otherText.value = '';
     }
